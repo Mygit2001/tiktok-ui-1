79b958638b418f67b1c9ac46d16e50ddc1435d28
Syle2001ht <77475693+Mygit2001@users.noreply.github.com>
2022-07-27 23:00:29 +0700

    upload code header 1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,28 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
 function Header() {
    return (
       <header className={cx('wrapper')}>
-         <div className={cx('inner')}></div>
+         <div className={cx('inner')}>
+            <div>
+               <img src={images.logo} alt="logo tiktok" />
+            </div>
+            <div className={cx('search')}>
+               <input placeholder="Tìm kiếm tài khoản và video" spellcheck={false} />
+               <button className={cx('clear')}>
+                  <FontAwesomeIcon icon={faCircleXmark} />
+               </button>
+               <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+               <button className={cx('search-btn')}>
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+               </button>
+            </div>
+            <div className={cx('actions')}>action</div>
+         </div>
       </header>
    );
 }
