89be1aa01c2ab887ed46a0b080a90d9b042f2651
Syle2001ht <77475693+Mygit2001@users.noreply.github.com>
2022-07-29 08:43:30 +0700

    upload code header 2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,26 +1,54 @@
 import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+import { useEffect, useState } from 'react';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import 'tippy.js/dist/tippy.css'; // optional
 import styles from './Header.module.scss';
 import images from '~/assets/images';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { Wrapper as PoperWrapper } from '~/components/Poper';
+import AccountItem from '~/components/AccountItem';
 const cx = classNames.bind(styles);
 function Header() {
+   const [searchResults, setSearchResults] = useState([]);
+   useEffect(() => {
+      setTimeout(() => {
+         setSearchResults([]);
+      }, 0);
+   }, []);
    return (
       <header className={cx('wrapper')}>
          <div className={cx('inner')}>
             <div>
                <img src={images.logo} alt="logo tiktok" />
             </div>
-            <div className={cx('search')}>
-               <input placeholder="Tìm kiếm tài khoản và video" spellcheck={false} />
-               <button className={cx('clear')}>
-                  <FontAwesomeIcon icon={faCircleXmark} />
-               </button>
-               <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-               <button className={cx('search-btn')}>
-                  <FontAwesomeIcon icon={faMagnifyingGlass} />
-               </button>
-            </div>
+            <Tippy
+               interactive
+               visible={searchResults.length > 0}
+               render={(attrs) => (
+                  <div className={cx('search-results')} tabIndex="-1" {...attrs}>
+                     <PoperWrapper>
+                        <h4 className={cx('search-title')}>Tài khoản</h4>
+                        <AccountItem />
+                        <AccountItem />
+                        <AccountItem />
+                        <AccountItem />
+                     </PoperWrapper>
+                  </div>
+               )}
+            >
+               <div className={cx('search')}>
+                  <input placeholder="Tìm kiếm tài khoản và video" spellcheck={false} />
+                  <button className={cx('clear')}>
+                     <FontAwesomeIcon icon={faCircleXmark} />
+                  </button>
+                  <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                  <button className={cx('search-btn')}>
+                     <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  </button>
+               </div>
+            </Tippy>
             <div className={cx('actions')}>action</div>
          </div>
       </header>
