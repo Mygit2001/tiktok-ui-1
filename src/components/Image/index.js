80fbc2c9facaa23795ec2cc0f227ef902da7c559
Syle2001ht <77475693+Mygit2001@users.noreply.github.com>
2022-08-16 21:16:04 +0700

    ap dung propstype

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,23 +1,31 @@
+import PropTypes from 'prop-types';
 import { useState, forwardRef } from 'react';
 import images from '~/assets/images';
 import classNames from 'classnames';
 import styles from './Image.module.scss';
 
-const Image = forwardRef(({ src, alt, className, ...props }, ref) => {
-   const [fallBack, setFallback] = useState('');
-   const handleError = () => {
-      setFallback(images.noImage);
-   };
-   return (
-      <img
-         className={classNames(styles.wrapper, className)}
-         ref={ref}
-         {...props}
-         alt={alt}
-         src={fallBack || src}
-         onError={handleError}
-      />
-   );
-});
-
+const Image = forwardRef(
+   ({ src, alt, className, fallBack: customFallback = images.noImage, ...props }, ref) => {
+      const [fallBack, setFallback] = useState('');
+      const handleError = () => {
+         setFallback(images.noImage);
+      };
+      return (
+         <img
+            className={classNames(styles.wrapper, className)}
+            ref={ref}
+            {...props}
+            alt={alt}
+            src={fallBack || src}
+            onError={handleError}
+         />
+      );
+   },
+);
+Image.propTypes = {
+   src: PropTypes.string,
+   alt: PropTypes.string,
+   className: PropTypes.string,
+   fallBack: PropTypes.string,
+};
 export default Image;
--- src/components/Poper/Menu/index.js
+++ src/components/Poper/Menu/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 import { useState } from 'react';
@@ -42,7 +43,7 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFuc
                <PoperWrapper className={cx('menu-popper')}>
                   {history.length > 1 && (
                      <Header
-                        title="Language"
+                        title={current.title}
                         onBack={() => {
                            setHistory((prev) => prev.slice(0, prev.length - 1));
                         }}
@@ -59,4 +60,10 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFuc
    );
 }
 
+Menu.propTypes = {
+   children: PropTypes.node.isRequired,
+   items: PropTypes.array,
+   hideOnClick: PropTypes.bool,
+   onChange: PropTypes.func,
+};
 export default Menu;
